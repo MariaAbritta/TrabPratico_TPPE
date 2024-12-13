31fc968574817d126ab24534ede52c893285f2e8
Maria Abritta <eduardaabritta@gmail.com>
2024-12-13 10:46:39 -0300

    Finalizando a triangulação e parametrização de TesteBaseCalculoImposto.java

--- TP1/src/tst/TesteBaseCalculoImposto.java
+++ TP1/src/tst/TesteBaseCalculoImposto.java
@@ -1,30 +1,53 @@
-// Objetivo: Desenvolvemos um teste que confirme a determinação da base de cálculo do imposto, que corresponde à diferença entre os ganhos tributáveis e as deduções (pensão alimentícia, contribuições para a previdência, deduções por dependentes, etc.).
-
 package tst;
 
 import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import app.IRPF;
 
+@RunWith(Parameterized.class)
 public class TesteBaseCalculoImposto {
 
-    IRPF irpf;
+    private IRPF irpf;
+    private float rendimento;
+    private float contribuicaoPrevidenciaria;
+    private float deducaoIntegral;
+    private float expectedBaseCalculo;
+
+    public TesteBaseCalculoImposto(float rendimento, float contribuicaoPrevidenciaria, float deducaoIntegral, float expectedBaseCalculo) {
+        this.rendimento = rendimento;
+        this.contribuicaoPrevidenciaria = contribuicaoPrevidenciaria;
+        this.deducaoIntegral = deducaoIntegral;
+        this.expectedBaseCalculo = expectedBaseCalculo;
+    }
 
     @Before
     public void setup() {
         irpf = new IRPF();
     }
 
-    @Test
-    public void testeBaseCalculoImposto() {
-      // Criamos um rendimento tributável de R$5000 e adicionamos deduções
-        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, 5000f);
-        irpf.cadastrarContribuicaoPrevidenciaria(1000f); // Dedução de 1000
-        irpf.cadastrarDeducaoIntegral("Funpresp", 500f); // Dedução adicional de 500
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            { 5000f, 1000f, 500f, 3500f }, // Caso 1
+            { 10000f, 3000f, 2000f, 5000f }, // Caso 2
+            { 7000f, 1500f, 800f, 4700f }, // Caso 3
+            { 4500f, 1200f, 300f, 3000f }, // Caso 4
+            { 3000f, 1000f, 1000f, 1000f }  // Caso 5
+        });
+    }
 
-        // Em seguida, validamos se a base de cálculo do imposto foi calculada corretamente, que é a diferença entre o rendimento e as deduções.
-        // Esperamos que a base de cálculo seja 5000 - 1000 - 500 = 3500
-        assertEquals(3500f, irpf.getBaseCalculoImposto(), 0f);
+    @Test
+    public void testeBaseCalculoImpostoParametrizado() {
+        irpf.criarRendimento("Salário", IRPF.TRIBUTAVEL, rendimento);
+        irpf.cadastrarContribuicaoPrevidenciaria(contribuicaoPrevidenciaria);
+        irpf.cadastrarDeducaoIntegral("Funpresp", deducaoIntegral);
+
+        assertEquals(expectedBaseCalculo, irpf.getBaseCalculoImposto(), 0.01f);
     }
 }
